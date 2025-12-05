030c6df53933793bb5d646bbaa509d92503d43d5
Marco Bacci <marcobacci94@gmail.com>
2025-12-05 14:58:03 +0100

    Fix: unify exercises in same app

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,19 @@
-// import useSwitch from "./Hooks/useSwitch";
-
-// function App() {
-//   const [isOn, toggle] = useSwitch();
-
-//   return (
-//     <div>
-//       <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
-//       <button onClick={toggle}>Cambia Stato</button>
-//     </div>
-//   );
-// }
-
-// export default App;
-// -------------------------------------------------------
+import useSwitch from "./Hooks/useSwitch";
 import useDate from "./Hooks/useDate";
 
 function App() {
+  const [isOn, toggle] = useSwitch();
   const currentDate = useDate();
 
   return (
     <div>
+      <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
+      <button onClick={toggle}>Cambia Stato</button>
+      {/* ------------------------------------------------------- */}
       <h1>Data e ora attuali:</h1>
       <p>{currentDate.toLocaleString()}</p>
     </div>
+
   );
 }
 
