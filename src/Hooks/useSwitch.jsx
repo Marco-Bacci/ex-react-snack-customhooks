7fa02910af4e203db6873f2cbcc993de130a38b8
Marco Bacci <marcobacci94@gmail.com>
2025-12-05 14:25:30 +0100

    Feat: snack 1 completed

---
+++ src/Hooks/useSwitch.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const useSwitch = (initialValue = false) => {
+  const [isOn, setIsOn] = useState(initialValue);
+  const toggle = () => {
+    setIsOn((curr) => !curr);
+  };
+  return [isOn, toggle];
+};
+
+export default useSwitch;
